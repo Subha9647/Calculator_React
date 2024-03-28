2d8743711916245e7d1f0b6e9f3fb759e5aa2d72
Subha Mondal <subha.snt2002@gmail.com>
2024-03-28 11:09:04 +0530

    Comlete calculator Using React

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import styles from "./App.module.css";
+import Display from "./components/Display";
+import ButtonContainer from "./components/ButtonContainer";
+
+function App() {
+  const [calVal, setCalVal] = useState("");
+
+  const onButtonClick = (buttonText) => {
+    if (buttonText === "C") {
+      setCalVal("");
+    } else if (buttonText === "=") {
+      const result = eval(calVal);
+      setCalVal(result);
+    } else {
+      const newDisplayValue = calVal + buttonText;
+      setCalVal(newDisplayValue);
+    }
+  };
+  return (
+    <div className={styles.calculator}>
+      <Display displayValue={calVal} />
+      <ButtonContainer onButtonClick={onButtonClick} />
+    </div>
+  );
+}
+
+export default App;
